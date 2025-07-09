83c6a383db866d1dcec775d3327be826d1ef9412
tesema <tesema.heyii21@gmail.com>
2025-07-09 08:24:55 +0300

    main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,27 @@
+interface Teacher {
+   readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any; // Allows additional properties
+
+}
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  
+  // should print
+  // Object
+  // contract: false
+  // firstName: "John"
+  // fullTimeEmployee: false
+  // lastName: "Doe"
+  // location: "London"
+  
